d7d48d54bd93104618f8d4919dbfd21f97d07703
mrb <michaelrbernstein@gmail.com>
2011-12-23 09:47:50 -0500

    files

---
+++ project.clj
@@ -0,0 +1,4 @@
+(defproject pianist "1.0.0-SNAPSHOT"
+  :description "redis rdb parser"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [gloss "0.2.1-SNAPSHOT"]])
---
+++ src/pianist/core.clj
@@ -0,0 +1,55 @@
+; Import the Java libs necessary for byte seq'ing
+(ns pianist.core
+  (:import (java.io FileOutputStream FileInputStream FileReader File)
+    (java.nio Buffer ByteBuffer CharBuffer)))
+
+; Define the record structure
+(defrecord DataTest [numbytes predicate])
+
+; Make some datatests
+(def magic
+  (DataTest. 5, #(= % '(82 69 68 73 83))))
+(def dumpversion
+  (DataTest. 4, #(= % '(48 48 48 50))))
+
+; Function to hold structure of records, probably not idiomatic
+(defn parsemap []
+  [magic dumpversion])
+
+; Inside a with-open, assembles a byte-array,
+; reads from a stream, and returns a seq
+(defn grab-bytes [stream bytes]
+  (let [ary (byte-array bytes)]
+    (.read stream ary)
+      (seq ary)))
+
+; Does the magic number and dump version number match
+; the map?
+(defn is-redis-file? [stream dataseq]
+  (= [true true]
+    (into []
+      (map
+        #(let [sbytes (:numbytes %) predicate (:predicate %)]
+          (predicate
+            (grab-bytes stream sbytes))) dataseq))))
+
+; Parse the body of the data file
+(defn parse-body [stream dataseq]
+  (let [ary (byte-array (.available stream))]
+    (.read stream ary)
+      (seq ary)))
+
+; The main interface function
+(defn parse-rdb-file [path dataseq]
+  (with-open [#^FileInputStream stream (FileInputStream. path)]
+    (cond
+      (is-redis-file? stream dataseq) (parse-body stream dataseq)
+      :else "Not a valid Redis .rdb file")))
+
+; Calling the function with a path and parsemap
+(parse-rdb-file "/Users/mrb/dump.rdb" (parsemap))
+
+(count (map #(bit-test % 0) (parse-rdb-file "/Users/mrb/dump.rdb" (parsemap))))
+
+  
+
