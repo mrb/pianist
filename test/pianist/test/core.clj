e361774a296c99265a9429508a79232e4afdef9d
mrb <michaelrbernstein@gmail.com>
2011-12-25 13:55:22 -0500

    tests working with lein test, should look into midje

--- test/pianist/test/core.clj
+++ test/pianist/test/core.clj
@@ -2,12 +2,12 @@
   (:use [pianist.core])
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false "No tests have been written."))
+(deftest is-a-valid-dump-file
+  (parse-rdb-file "test/fixtures/dump.rdb" (parsemap))
+  (is true))
 
-;; Calling the function with a path and parsemap
-(parse-rdb-file "test/fixtures/dump.rdb" (parsemap))
+(deftest is-not-a-valid-dump-file
+  (parse-rdb-file "test/fixtures/not_a_dump.rdb" (parsemap))
+  (is "Not a valid Redis .rdb file"))
 
-(parse-rdb-file "test/fixtures/not_a_dump.rdb" (parsemap))
-
-(count (map #(bit-test % 0) (parse-rdb-file "test/fixtures/dump.rdb" (parsemap))))
+;;(count (map #(bit-test % 0) (parse-rdb-file "test/fixtures/dump.rdb" (parsemap))))
